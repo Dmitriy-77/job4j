160db599b981758a34055cc0c4cf11a3d65da623
Your Name <you@example.com>
2019-12-02 10:38:57 +0300

    Максимум из двух чисел 4.2

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -2,8 +2,8 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left;
-        return result > right ? left : right;
+
+        return left > right ? left : right;
 
     }
 }
