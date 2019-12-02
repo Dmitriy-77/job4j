a0412a427cd917522eec97e21b157d4dddc48dd0
Your Name <you@example.com>
2019-12-02 21:57:33 +0300

    Отладка программы в IDEA 4.5

---
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SqMaxTest {
+    @Test
+    public void whenForthMax() {
+        int result = SqMax.max(5,6,3,4);
+        assertThat(result,is(6));
+    }
+
+    @Test
+    public void whenForthMax2() {
+        int result = SqMax.max(7,6,3,4);
+        assertThat(result,is(7));
+    }
+
+    @Test
+    public void whenForthMax3() {
+        int result = SqMax.max(5,6,9,4);
+        assertThat(result,is(9));
+    }
+}
