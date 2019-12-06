534d685cd74eae40540590d817ee12cad6115854
Your Name <you@example.com>
2019-12-07 01:03:39 +0300

    Простое число 5.5 + Test

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,7 +3,11 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public boolean check(int finish) {
         boolean prime = false;
-
+        for(int i = 2; i < finish; i++) {
+            if( (finish%i) != 0) {
+                prime = true;
+            }else break;
+        }
         return prime;
     }
 }
